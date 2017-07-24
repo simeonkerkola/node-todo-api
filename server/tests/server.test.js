76c2091b26b23cbf079cc9853eb46093bae574c8
simi <simeon.kerkola@gmail.com>
2017-07-24 13:14:38 +0300

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,16 @@ const request = require('supertest')
 const {app} = require('./../server')
 const {Todo} = require('./../models/todo')
 
+const todos = [{
+  text: 'First text todo'
+}, {
+  text: 'Second test todo'
+}]
+
 // clears database before each test case
 beforeEach((done) => {
   Todo.remove({}) // wipes out all todos
+  .then(() => Todo.insertMany(todos)) // insert our arrays
   .then(() => done())
 })
 
@@ -24,7 +31,7 @@ describe('POST /todos', () => {
     .end((err, res) =>{
       if (err) done(err)
 
-      Todo.find().then((todos) => {
+      Todo.find({text}).then((todos) => {
         expect(todos.length).toBe(1)
         expect(todos[0].text).toBe(text)
         done()
@@ -41,9 +48,21 @@ describe('POST /todos', () => {
       if (err) done(err)
 
       Todo.find().then((todos) => {
-        expect(todos.length).toBe(0)
+        expect(todos.length).toBe(2)
         done()
       }).catch((e) => done(e))
     })
+  })
+})
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2)
+      })
+      .end(done)
   })
 })
