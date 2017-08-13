b0f87a3844bf12ceef54573d6e6d43da92eddb60
sssmi <simeon.kerkola@gmail.com>
2017-08-13 10:25:00 +0300

    Update config to work with Heroku as well

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# node-todo-api
+# Lemon tree Todo
+
+### Create new Todo tasks and complete them
+**Connect with postman: https://lemon-tree-todo.herokuapp.com/**
+
+A Nodejs base app with user login system for managing stuff you need to do
+
+### Use with Postman
+* Connect with postman
--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,10 @@
 const env = process.env.NODE_ENV || 'development'
-const config = require('./config.json')
+
 
 console.log('env *****', env)
 
 if (env === 'development' || env === 'test') {
+  const config = require('./config.json') // eslint-disable-line global-require
   const envConfig = config[env]
 
   // Object.keys() returns an array of all the keys
