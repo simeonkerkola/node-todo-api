53de9bd96ec0eafcffd8868ac88176d3e5539773
sssmi <simeon.kerkola@gmail.com>
2017-09-02 12:50:50 +0300

    Refactor to async

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,5 @@
 const env = process.env.NODE_ENV || 'development'
 
-
 console.log('env *****', env)
 
 if (env === 'development' || env === 'test') {
--- server/server.js
+++ server/server.js
@@ -15,73 +15,77 @@ const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 
-app.post('/todos', authenticate, (req, res) => {
-  const todo = new Todo({
-    text: req.body.text,
-    _creator: req.user._id,
-  })
-
-  todo.save().then((doc) => {
-    res.send(doc)
-  }, e => res.status(400).send()) // https://httpstatuses.com/
+app.post('/todos', authenticate, async (req, res) => {
+  try {
+    const todo = new Todo({
+      text: req.body.text,
+      _creator: req.user._id,
+    })
+    const doc = await todo.save()
+    return res.send(doc)
+  } catch (e) {
+    return res.status(400).send() // https://httpstatuses.com/
+  }
 })
 
-app.get('/todos', authenticate, (req, res) => {
-  Todo.find({
-    _creator: req.user._id,
-  }).then((todos) => {
-    res.send({
-      todos,
+app.get('/todos', authenticate, async (req, res) => {
+  try {
+    const todos = await Todo.find({
+      _creator: req.user._id,
     })
-  }, e => res.status(400).send(e))
+    res.send({ todos })
+  } catch (e) {
+    res.status(400).send(e)
+  }
 })
 
 // GET /todos/12345
-app.get('/todos/:id', authenticate, (req, res) => {
-  const id = req.params.id
+app.get('/todos/:id', authenticate, async (req, res) => {
+  try {
+    const id = req.params.id
 
-  // Valid id using isValid
-  // if not found res 404 - send back empty body
-  if (!ObjectID.isValid(id)) res.status(404).send({})
-
-  Todo.findOne({
-    _id: id,
-    _creator: req.user._id,
-  }).then((todo) => {
-    // if no todo - send back 404 with empty body
-    if (!todo) res.status(404).send() // success
-    res.send({
-      todo,
+    // Valid id using isValid
+    // if not found res 404 - send back empty body
+    if (!ObjectID.isValid(id)) return res.status(404).send({})
+    const todo = await Todo.findOne({
+      _id: id,
+      _creator: req.user._id,
     })
-  }).catch(e => res.status(400).send(e)) // error 400 - send empty body back
+
+    // if no todo - send back 404 with empty body
+    if (!todo) return res.status(404).send({})
+    return res.send({ todo })
+  } catch (e) {
+    return res.status(400).send(e)
+  }
 })
 
-app.delete('/todos/:id', authenticate, (req, res) => {
-  // get the id
-  const id = req.params.id
-  // validate the id, if not -> 404
-  if (!ObjectID.isValid(id)) res.status(404).send()
+app.delete('/todos/:id', authenticate, async (req, res) => {
+  try {
+    // get the id
+    const id = req.params.id
+    // validate the id, if not -> 404
+    if (!ObjectID.isValid(id)) throw new Error()
+    const todo = await Todo.findOneAndRemove({
+      _id: id,
+      _creator: req.user._id,
+    })
+    if (!todo) throw new Error()
 
-  Todo.findOneAndRemove({
-    _id: id,
-    _creator: req.user._id,
-  }).then((todo) => {
-    // if doc no deleted send 404
-    if (!todo) return res.status(404).send()
-
-    // if deleted send back doc with 200
-    res.send({ todo, })
-  }).catch(e => res.status(400).send())
+    return res.send({ todo })
+  } catch (e) {
+    return res.status(404).send()
+  }
 })
 
-// update
-app.patch('/todos/:id', authenticate, (req, res) => {
+// PATCH /todos update
+app.patch('/todos/:id', authenticate, async (req, res) => {
   const id = req.params.id
 
   // picks these two arguments and only if they exist
   const body = _.pick(req.body, ['text', 'completed'])
 
-  if (!ObjectID.isValid(id)) return res.status(404).send()
+  if (!ObjectID.isValid(id)) res.status(404).send()
 
   if (_.isBoolean(body.completed) && body.completed) {
     body.completedAt = new Date().getTime()
@@ -90,32 +94,36 @@ app.patch('/todos/:id', authenticate, (req, res) => {
     body.completedAt = null
   }
 
-  // findOneAndUpdate
-  Todo.findOneAndUpdate({
-    _id: id,
-    _creator: req.user._id,
-  }, {
-    $set: body,
-  }, {
-    new: true,
-  }).then((todo) => {
-    if (!todo) return res.status(404).send()
-
-    res.send({ todo, })
-  }).catch(e => res.status(400).send())
+  try {
+    // findOneAndUpdate
+    const todo = await Todo.findOneAndUpdate({
+      _id: id,
+      _creator: req.user._id,
+    }, {
+      $set: body,
+    }, {
+      new: true,
+    })
+    if (!todo) res.status(404).send()
+    return res.send({ todo })
+  } catch (e) {
+    return res.status(400).send()
+  }
 })
 
 
 // POST /users
-
-app.post('/users', (req, res) => {
-  // pick only email and password
-  const body = _.pick(req.body, ['email', 'password'])
-  const user = new User(body)
-
-  user.save().then(() => user.generateAuthToken()).then((token) => {
+app.post('/users', async (req, res) => {
+  try {
+    // pick only email and password
+    const body = _.pick(req.body, ['email', 'password'])
+    const user = new User(body)
+    await user.save()
+    const token = await user.generateAuthToken()
     res.header('x-auth', token).send(user)
-  }).catch(e => res.status(400).send(e))
+  } catch (e) {
+    res.status(400).send(e)
+  }
 })
 
 // use a callback authenticate (at middleware/auhenticate) as a middleware/pre-condition
@@ -124,24 +132,24 @@ app.get('/users/me', authenticate, (req, res) => {
 })
 
 // POST /users/login {email, password}
-app.post('/users/login', (req, res) => {
-  const body = _.pick(req.body, ['email', 'password'])
-
-  User.findByCredentials(body.email, body.password).then((user) => {
-    user.generateAuthToken().then((token) => {
-      res.header('x-auth', token).send(user)
-    })
-  }).catch((e) => {
+app.post('/users/login', async (req, res) => {
+  try {
+    const body = _.pick(req.body, ['email', 'password'])
+    const user = await User.findByCredentials(body.email, body.password)
+    const token = await user.generateAuthToken()
+    res.header('x-auth', token).send(user)
+  } catch (e) {
     res.status(400).send()
-  })
+  }
 })
 
-app.delete('/users/me/token', authenticate, (req, res) => {
-  req.user.removeToken(req.token).then(() => {
-    res.status(200).send()
-  }, () => {
-    res.status(400).send()
-  })
+app.delete('/users/me/token', authenticate, async (req, res) => {
+  try {
+    await req.user.removeToken(req.token)
+    return res.status(200).send()
+  } catch (e) {
+    return res.status(400).send()
+  }
 })
 
 
